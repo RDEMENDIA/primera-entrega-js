315f628ab3547f41a87c3a488f6b9085b56c99c7
Rodrigo De Mendia <rdemendia@icloud.com>
2022-03-02 02:08:35 +0100

    practica

--- proyectoJs.js
+++ proyectoJs.js
@@ -1,50 +1,51 @@
 alert("Bienvenido a nuestro sitio, aqui podras encontrar: \n-Yerba \n-Mantecol \n-Dulce de leche \n-Alfajores")
+
 /* let nombreProductoA = 'Yerba';
 let stockProductoA = 10;
 let precioProductoA = 75; */
 
-// objeto
+/* // objeto
 const productoA = {
     nombre: 'yerba',
     stock: 10,
     precio: 75
-} 
+}  */
 
 /* let nombreProductoB = 'Mantecol';
 let stockProductoB = 8;
 let precioProductoB = 30; */
 
-// objeto
+/* // objeto
 const productoB = {
     nombre: "mantecol",
     stock: 8,
     precio: 55
-}
+} */
 
 /* let nombreProductoC = 'Dulce de leche';
 let stockProductoC = 3;
 let precioProductoC = 80; */
 
 // objeto
-const productoC = {
+/* const productoC = {
     nombre: "dulce de leche",
     stock: 3,
     precio: 80
-}
+} */
 
 /* let nombreProductoD = 'Alfajores';
 let stockProductoD = 10;
 let precioProductoD = 30; */
 
 // objeto
-const productoD = {
+/* const productoD = {
     nombre: "alfajores",
     stock: 10,
     precio: 30
-}
+} */
 
 // agregar los productos en un Array
-const listaProductos = [productoA, productoB, productoC, productoD]
+const listaProductos = []
 
 // funcion constructora para crear un nuevo objeto
 function Producto (nombreValor, stockValor, precioValor){
@@ -54,9 +55,17 @@ function Producto (nombreValor, stockValor, precioValor){
 }
 
 // asi se crearia un nuevo objeto
+const productoA = new Producto("yerba", 10, 75)
+const productoB = new Producto("mantecol", 8, 55)
+const productoC = new Producto("dulce de leche", 3, 80)
+const productoD = new Producto("alfajores", 10, 30)
 const productoE = new Producto("tita", 10, 20)
 
 // asi se agregaria a la listaProductos
+listaProductos.push(productoA)
+listaProductos.push(productoB)
+listaProductos.push(productoC)
+listaProductos.push(productoD)
 listaProductos.push(productoE)
 
 
@@ -92,7 +101,7 @@ function stockSuficiente(stock, precio, nombre) {
 
 }
 
-while ((nombreCompra != productoA.nombre) && (nombreCompra != productoB.nombre) && (nombreCompra != productoC.nombre) &&  (nombreCompra != productoD.nombre)){
+while ((nombreCompra != productoA.nombre) && (nombreCompra != productoB.nombre) && (nombreCompra != productoC.nombre) &&  (nombreCompra != productoD.nombre) && (nombreCompra != productoE.nombre)){
     nombreCompra = prompt("Que producto desea comprar?").toLowerCase();
     if(nombreCompra == productoA.nombre) {
         compra(productoA.stock, productoA.precio, productoA.nombre);
@@ -106,6 +115,12 @@ while ((nombreCompra != productoA.nombre) && (nombreCompra != productoB.nombre) 
     }
     else if(nombreCompra == productoD.nombre) {
         compra(productoD.stock, productoD.precio, productoD.nombre);        
+    }
+    else if(nombreCompra == "alfajor") {
+        compra(productoD.stock, productoD.precio, productoD.nombre);        
+    }
+    else if(nombreCompra == productoE.nombre) {
+        compra(productoE.stock, productoE.precio, productoE.nombre);        
     }
     else {
         alert("Asegurese de escribir bien el nombre del producto");
@@ -127,11 +142,16 @@ if (quiereMas == "Si") {
 //     {nombre: 'ReactJS', precio: 22000},
 // ]
 
-const resultado = listaProductos.find((Producto) => Producto.nombre === "alfajor")
-const resultado2 = listaProductos.find((el) => el.nombre === "Dulce")
+const resultado = listaProductos.find((el) => el.nombre === "tita")
+
+const resultado2 = listaProductos.filter((el) => el.nombre.includes(""))
 
 console.log(resultado) // {nombre: 'ReactJS', precio: 22000}
-console.log(resultado2) // undefined
+console.log(resultado2.nombre) // undefined
+
+// no esta del todo logrado
+alert(resultado.nombre)
+alert(resultado2.nombre)
 
 // const cursos = [
 //     {nombre: 'Javascript', precio: 15000},
